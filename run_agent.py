0c43376bb7a302c6970bd0e8c905592d209d20c5
Fernando <fernandocabrerabrz@gmail.com>
2025-08-24 21:21:22 -0500

    Primer commit

---
+++ run_agent.py
@@ -0,0 +1,30 @@
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from azure.ai.agents.models import ListSortOrder
+
+project = AIProjectClient(
+    credential=DefaultAzureCredential(),
+    endpoint="https://ia-analytics.services.ai.azure.com/api/projects/PoC",
+)
+
+agent = project.agents.get_agent("asst_XizkjMGP4EQaFZYnygjH8BET")
+
+thread = project.agents.threads.create()
+print(f"Created thread, ID: {thread.id}")
+
+message = project.agents.messages.create(
+    thread_id=thread.id, role="user", content="Hello Agent"
+)
+
+run = project.agents.runs.create_and_process(thread_id=thread.id, agent_id=agent.id)
+
+if run.status == "failed":
+    print(f"Run failed: {run.last_error}")
+else:
+    messages = project.agents.messages.list(
+        thread_id=thread.id, order=ListSortOrder.ASCENDING
+    )
+
+    for message in messages:
+        if message.text_messages:
+            print(f"{message.role}: {message.text_messages[-1].text.value}")
